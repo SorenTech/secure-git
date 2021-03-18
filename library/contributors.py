4b225085651905c9d84b9fc77a3f319334ed5f11
apmarshall <apmarshall@soren.tech>
2021-03-18 11:28:20 -0400

    scaffolding for contributor queries/mutations

---
+++ library/contributors.py
@@ -0,0 +1,14 @@
+# Query: List all the current contributors on a project
+get_current_contributors = {
+    
+}
+
+# Query: Check if proposed new contributor matches our security profile
+check_new_contributor = {
+    
+}
+
+# Mutation: Add a Contributor to our Project
+add_new_contributor = {
+    
+}
