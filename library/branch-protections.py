9f4df255dbc577c50abd1fc6cb2123d378af5170
Alex Floyd Marshall <apmarshall@soren.tech>
2021-04-03 21:15:14 -0400

    Allow review counts to be set as a variable

--- library/branch-protections.py
+++ library/branch-protections.py
@@ -41,13 +41,13 @@ get_branch_protections = {
 }
 
 update_branch_protections_main = {
-    updateBranchProtectionRule (input:{pattern:"main, master, release*, beta*", allowsDeletions:False, allowsForcePushes:False, isAdminEnforced:True, requiresApprovingReviews:True, requiredApprovingReviewsCount:1, requiresCodeOwnerReviews:True, restrictReviewDismissals:True, requireCommitSignatures:True, requiresLinearHistory:False, restrictPushes:True}) {
+    updateBranchProtectionRule (input:{pattern:"main, master, release*, beta*", allowsDeletions:False, allowsForcePushes:False, isAdminEnforced:True, requiresApprovingReviews:True, requiredApprovingReviewsCount:this.mainReviewsCount, requiresCodeOwnerReviews:True, restrictReviewDismissals:True, requireCommitSignatures:True, requiresLinearHistory:False, restrictPushes:True}) {
         branchProtectionRule
     }
 }
 
 update_branch_protections_develop = {
-    updateBranchProtectionRule (input:{pattern:"dev, develop", allowsDeletions:False, allowsForcePushes:False, requiresApprovingReviews:True, requiredApprovingReviewsCount:1, requireCommitSignatures:True, requiresLinearHistory:False, restrictPushes:True}) {
+    updateBranchProtectionRule (input:{pattern:"dev, develop", allowsDeletions:False, allowsForcePushes:False, requiresApprovingReviews:True, requiredApprovingReviewsCount:this.devReviewsCount, requireCommitSignatures:True, requiresLinearHistory:False, restrictPushes:True}) {
         branchProtectionRule
     }
 }
