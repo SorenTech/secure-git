af4d09be42c9353bc043ce7d26653e3cc9542e79
apmarshall <apmarshall@soren.tech>
2021-07-23 10:50:29 -0400

    Simplify to be just about the GitHub API graphql query

--- repositories/github_api_query.py
+++ repositories/github_api_query.py
@@ -1,12 +1,16 @@
 # Creates queries to the GitHub API and contructs an API object, per the adaptor, out of them.
 
 from .repo import Repository
-from .github_adaptor import GitHubObject as ghObject
 
 # Construct a GraphQL Query for the repo
 def graphql_query(repo: Repository):
 '''
-Queries the GitHub GraphQL API for info on a named repository and returns the results.
+Queries the GitHub GraphQL API for info on a named repository and returns the results:
+    - signedCommits: via Branch Protection Rules
+    - verifiedMerges: Percentage of the last 100 merge commits with valid signatures
+    - codeReviews: required for main branch via branch protection rules
+    - pullRequests: required for main branch (via branch protection rules)
+    - noForceMerges: blocked via branch protection rules
 '''
     return graphReturn
 
@@ -18,18 +22,3 @@ Returns a dictionary of values based on the results of the graphQL query.
     values = {}
     
     return values
-
-
-# Instantiate a ghObject with the keyword arguments
-def create_gh_object(repo: Repository):
-'''
-Creates a GitHub Object based on the results of a GraphQL query, uses that object to update a repository's state.
-'''
-    graphReturn = graphql_query(repo)
-    
-    values = create_values(repo, graphReturn)
-    
-    object = new ghObject(**values)
-
-    # Update the repo object based on ghObject
-    object.repo_update()
