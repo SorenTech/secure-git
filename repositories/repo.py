d6586f1a5170178b60fd865f8f65b245044e2a9a
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-24 15:59:27 -0400

    Substantial refactor of repo class
    Simplifying init method
    
    Adding additional fields and changing others to be automatically
    calculated from other fields
    
    Adding new methods for new fields to improve API

--- repositories/repo.py
+++ repositories/repo.py
@@ -9,7 +9,7 @@ class Repository:
         Stub: The User/Name or Org/Name portion of the URL (another str)
         Location: The URL of the repo (another str)
         Users: A dictionary of users authorized for the repo
-        Agents: A dictionary of authentication agents authorized for the repo
+        Agents: A dictionary of automation agents authorized for the repo
         Verification Status (hasVerification): Whether the repo meets the verification requirements of the script (bool)
         Scanning Status (hasScanning): Whether the repo meets the scanning requirements of the script (bool)
         Authorization Status (hasAuthorization): Whether the repo meets the protection/authorization requirements of the script (bool)
@@ -25,145 +25,170 @@ class Repository:
         self.location = location
         self.users = []
         self.agents = []
-        self.hasVerification = false
-        self.hasScanning = false
-        self.hasAuthorization = false
-        self.hasAuthentication = 0.0
-        self.verificationScore = 0.0
-        self.scanningScore = 0.0
-        self.authorizationScore = 0.0
-        self.authenticationScore = 0.0
+        self.hasSignedCommits = false
+        self.hasFullAttestation = false
+        self.hasSecretBlocking = false
+        self.hasSecurityScanning = false
+        self.hasCodeReviews = false
+        self.hasPullRequests = false
+        self.hasNoForceMerges = false
+        self.hasUserAuth = false
+        self.hasAgentAuth = false
+        self.hasVerification = self.hasSignedCommits && self.hasFullAttestation
+        self.hasScanning = self.hasSecretBlocking && self.hasSecurityScanning
+        self.hasAuthorization = self.hasCodeReviews && self.hasPullRequests && self.hasNoForceMerges
+        self.hasAuthentication = self.hasUserAuth && self.hasAgentAuth
+        self.signedCommitsScore = 0.0
+        self.fullAttestationScore = 0.0
+        self.secretBlockingScore = 0.0
+        self.securityScanningScore = 0.0
+        self.codeReviewsScore = 0.0
+        self.pullRequestsScore = 0.0
+        self.forceMergesScore = 0.0
+        self.userAuthScore = 0.0
+        self.agentAuthScore = 0.0
+        self.verificationScore = self.signedCommitScore + self.fullAttestationScore
+        self.scanningScore = self.secretBlockingScore + self.securityScanningScore
+        self.authorizationScore = self.codeReviewScore + self.pullRequestScore + self.forceMergesScore
+        self.authenticationScore = self.userAuthScore + self.agentAuthScore
         self.overallScore = Decimal.multiply(Decimal.add(verificationScore, scanningScore, protectionScore, authenticationScore), 1/4)
         
-# Use Case: Repo-Verification -> Commit Signatures are Required
-    def signature_verification(self, reqsSignedCommits: bool) -> float:
+# Use Case: Check if Commit Signatures are Required
+    def signatures_required(self, reqsSignedCommits: bool):
     '''
-    Updates the verificationScore value a repo based on whether that repo requires signed commits.
+    Updates the hasSignedCommits value a repo based on whether that repo requires signed commits.
     '''
         if (reqsSignedCommits == true):
-            self.verificationScore = Decimal.add(self.verificationScore, 0.5)
+            self.hasSignedCommits = true
+        else:
+            self.hasSignedCommits = false
             
-# Use Case: Repo-Verificattion -> Verified and Signed Merges (ie, Full Attestation)
-    def full_attestation(self, percentVerifiedMerges: float) -> float:
+# Use Case: Check % of Commits Signed
+    def signed_commits_percent(self, percentSignedCommits: float):
     '''
-    Updates the verificationScore value of the repo based on the percentage of merges in the repo which are verified and signed.
+    Updates the signedCommitsScore based on the percentage of commits to main branch which are signed.
+    '''
+        if (percentSignedCommits > 0):
+            self.signedCommitsScore = percentSignedCommits
+
+# Use Case: Check % of Verified and Signed Merges (ie, Full Attestation)
+    def full_attestation_percent(self, percentVerifiedMerges: float):
+    '''
+    Updates the fullAttestationScore value of the repo based on the percentage of merges in the repo which are verified and signed.
     '''
         if (percentVerifiedMerges > 0):
-            self.verificationScore = Decimal.add(self.verificationScore, Decimal.multiply(percentVerifiedMerges, 0.5))
+            self.fullAttestationScore = percentVerifiedMerges
+            
+# Use Case: Set hasFullAttestation value vased on fullAttestationScore
+    def full_attestation_bool(self):
+    '''
+    Sets the hasFullAttestation value to "true" if fullAttestationScore is greater than 75%
+    '''
+        if (fullAttestationScore >= 0.75):
+            self.hasFullAttestation = true
+        else:
+            self.hasFullAttestation = false
             
 # Use Case: Repo-Scanning -> Block Secrets Being Commmitted
-    def secret_blocking(self, hasSecretScanning: bool) -> float:
+    def secret_blocking(self, hasSecretScanning: bool):
     '''
-    Updates the scanningScore value of the repo based on whether or not automated scanning exists to block the committing of secret files to the repository.
+    Updates the hasSecretBlocking and secretBlockingScore value based on whether or not automated scanning exists to block the committing of secret files to the repository.
     '''
         if (hasSecretScanning == true):
-            self.scanningScore = Decimal.add(self.scanningScore, 0.5)
+            self.hasSecretBlocking = true
+            self.secretBlockingScore = 1.0
+        else:
+            self.hasSecretBlocking = false
+            self.secretBlockingScore = 0.0
 
 ## Use Case: Repo-Scanning -> Automatic Security Scanning
-    def security_scanning(self, hasSecurityScanning: bool) -> float:
+    def security_scanning(self, securityScanning: bool):
     '''
-    Updates the scanningScore value of the repo based on whether or not automated security scanning is performed.
+    Updates the hasSecurityScanning and securityScanningScore value of the repo based on whether or not automated security scanning is performed.
     '''
-        if (hasSecurityScanning == true):
-            self.scanningScore = Decimal.add(self.scanningScore, 0.5)
+        if (securityScanning == true):
+            self.hasSecurityScanning = true
+            self.securityScanningScore = 1.0
+        else:
+            self.hasSecurityScanning = false
+            self.securityScanningScore = 0.0
 
 # Use Case: Repo-Authorizations -> Require Code Reviews
-    def req_code_reviews(self, reqsCodeReviews: bool) -> float:
+    def req_code_reviews(self, reqsCodeReviews: bool):
     '''
-    Updates the authorizationScore value of the repo based on whether code reviews are required for merges into the main branch.
+    Updates the hasCodeReviews and codeReviewsScore value of the repo based on whether code reviews are required for merges into the main branch.
     '''
         if (reqsCodeReviews == true):
-            self.authorizationScore = Decimal.add(self.authorizationScore, 1/3)
+            self.hasCodeReviews = true
+            self.codeReviewsScore = 1.0
+        else:
+            self.hasCodeReviews = false
+            self.codeReviewsScore = 0.0
 
 # Use Case: Repo-Authorization -> Require Pull Requests on Main
-    def req_pull_requests(self, reqsPullRequests: bool) -> float:
+    def req_pull_requests(self, reqsPullRequests: bool):
     '''
-    Updates the authorizationScore value of the repo based on whether pull requests are required to merge code into the main branch.
+    Updates the hasPullRequests and pullRequestsScore values of the repo based on whether pull requests are required to merge code into the main branch.
     '''
         if (reqsPullRequests == true):
-            self.authorizationScore = Decimal.add(self.authorizationScore, 1/3)
+            self.hasPullRequests = true
+            self.pullRequestsScore = 1.0
+        else:
+            self.hasPullRequests = false
+            self.pullRequestsScore = 0.0
 
 # Use Case: Repo-Authorizations -> Prevent Force-Merges 
-    def block_force_merges(self, noForceMerges: bool) -> float:
+    def block_force_merges(self, noForceMerges: bool):
     '''
-    Updates the authorizationScore value of the repo based on whether forced merges are blocked for all branches.
+    Updates the hasNoForceMerges and forceMergesScore values of the repo based on whether forced merges are blocked for all branches.
     '''
         if (noForceMerges == true):
-            self.authorizationScore = Decimal.add(self.authorizationScore, 1/3)
+            self.hasNoForceMerges = true
+            self.forceMergesScore = 1.0
+        else:
+            self.hasNoForceMerges = false
+            self.forceMergesScore = 0.0
 
 # Use Case: Repo-Authentication -> Users Meet Authentication Requirements
-    def user_authentication_reqs(self, users: dict) -> float:
+    def user_authentication_score(self, users: dict):
     '''
-    Updates the authenticationScore value of the repo based on the number of users who meet the authentication requirements.
+    Updates the userAuthScore value of the repo based on the number of users who meet the authentication requirements.
     '''
         userAuthScores = []
         userCount = len(users)
         for user in users:
             if (user.AuthMethodsApproved == True):
                 userAuthScores + 1
-        self.authenticationScore = Decimal.add(self.authenticationScore, Decimal.multiply(userAuthScores, 1/userCount, 0.5))
+        self.userAuthScore = Decimal.multiply(userAuthScores, 1/userCount)
+        
+# Use Case: Agent Auth Score -> Agent Auth Bool
+    def user_authentication_bool(self):
+    '''
+    Updates the hasUserAuth value if userAuthScore is greater than or equal to 75%
+    '''
+        if (self.userAuthScore >= 0.75):
+            self.hasUserAuth = true
+        else:
+            self.hasUserAuth = false
 
 # Use Case: Repo-Authentication -> Agents Meet Authentication Requirements
-    def agent_authentication_reqs(self, agents: dict) -> float:
+    def agent_authentication_score(self, agents: dict):
     '''
-    Updates the authenticationScore value of the repo based on the number of automation agents who meet the authentication requirements.
+    Updates the agentAuthScore value of the repo based on the number of automation agents who meet the authentication requirements.
     '''
         agentAuthScores = []
         agentCount = len(agents)
         for agent in agents:
             if (agent.AuthMethodsApproved == True):
                 agentAuthScores + 1
-            self.authenticationScore = Decimall.add(self.authenticationScore, Decimal.multiply(agentAuthScores, 1/agentCount, 0.5))
-
-# Use Case: Update repo bool values based on decimal scores
-    def verification_status(self):
+            self.authenticationScore = Decimall.multiply(agentAuthScores, 1/agentCount)
+            
+# Use Case: Agent Authentication Score -> Agent Auth Bool
+    def agent_authentication_bool(self):
     '''
-    Updates the hasVerification state of the repo based on its verificationScore
+    Updates the hasAgentAuth value to true if the agentAuthScore is greater than or equal to 90%
     '''
-        if (self.verificationScore == 1):
-            self.hasVerification = true
-        else: self.hasVerification = false
-        
-    def scanning_status(self):
-    '''
-    Updates the hasScanning state of the repo based on its scanningScore
-    '''
-        if (self.scanningScore == 1):
-            self.hasScanning = true
-        else: self.hasScanning = false
-    
-    def authorization_status(self):
-    '''
-    Updates the hasAuthorization state of the repo based on its authorizationScore
-    '''
-        if (self.authorizationScore == 1):
-            self.hasAuthorization = true
-        else: self.hasAuthorization = false
-
-    def authentication_status(self):
-    '''
-    Updates the hasAuthentication state of the repo based on its authenticationScore
-    '''
-        if (self.authenticaitonScore == 1):
-            self.hasAuthenticaiton = true
-        else: self.hasAuthenticaiton = false
-
-    def status_check(sef):
-    '''
-    Updates all boolean flags.
-    '''
-        verification_status()
-        scanning_status()
-        authorization_status()
-        authentication_status()
-
-# Use Case: Reset all scores to zero and recompute booleans before an update
-    def repo_score_reset(self):
-    '''
-    Reset all scores to zero and all booleans to false. Can be used prior to an ensure values all add up.
-    '''
-        self.verificationScore = 0
-        self.scanningScore = 0
-        self.authorizationScore = 0
-        self.authenticationScore = 0
-        status_check()
+        if (self.agentAuthScore >= 0.9):
+            self.hasAgentAuth = true
+        else:
+            self.hasAgentAuth = false
