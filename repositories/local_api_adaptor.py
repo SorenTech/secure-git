8b0aa0a4553216e05347b8273d285379e84d983e
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-24 06:51:16 -0400

    Local API Adaptor - skeleton file

---
+++ repositories/local_api_adaptor.py
@@ -0,0 +1,14 @@
+from repo import Repository
+from OS import File
+
+'''
+Translates information gleaned from the local git file system and configuration to Repository object fields.
+'''
+
+class localObject:
+    def __init__(self):
+    '''
+    '''
+        # init here
+    
+    
