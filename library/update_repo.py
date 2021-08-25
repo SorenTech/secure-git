9018f408dbf20e3104d9a48b11a73a94d45b5ff1
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 14:57:20 -0400

    Comment syntax

--- library/update_repo.py
+++ library/update_repo.py
@@ -1,3 +1,14 @@
-'''
-Based on repo scoring and bool values, runs commands to change repo settings in line with requirements.
-'''
+# Based on repo scoring and bool values, runs commands to change repo settings in line with requirements.
+
+import repositories
+
+# Step 1: Run check_repo
+
+# Step 2: Construct the API request
+# - construct a mutation/api request based on which elements register "false" in the repo object
+
+## To-do: make an option to do this command by command so users can select which items they wish to remediate
+
+# Step 3: Run the API request
+
+# Step 4: Report the results
