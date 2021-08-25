7034ed1ac42572a9e58444720cf67c6d15e2954b
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 14:14:51 -0400

    Updating adaptor based on what can be gleaned from GitHub, revised repo class

--- repositories/github_api_adaptor.py
+++ repositories/github_api_adaptor.py
@@ -1,55 +1,46 @@
-from .repo import Repository
+# Translates GitHub API return values into Repository object fields.
 
-'''
-Translates GitHub API return values into Repository object fields.
-'''
+from . import Repository
 
 # Intermediary API Object:
 class GHObject:
-    def __init__(self, repo: Repository, signedCommits: bool, verfiedMerges: float, secretBlocking: bool, securityScanning: bool, codeReviews: bool, pullRequests: bool, noForceMerges: bool):
+    def __init__(self, repo: Repository, users: dict, agents: dict, signedCommits: bool, actualSignedCommits: float, actualVerifiedMerges: float, codeReviews: float, pullRequests: float, blockForceMerges: float):
     '''
     Defines an object that (a) references a repo object (the repo being examined) and (b) contains the results of a GitHub API request in the format we need to populate the values of the repo through the Repository class's included methods.
     '''
         self.repo = repo
+		self.users = users
+		self.agents = agents
         self.signedCommits = signedCommits
-        self.verifiedMerges = verifiedMerges
-        self.secretBlocking = secretBlocking
-        self.securityScanning = securityScanning
+        self.actualSignedCommits = actualSignedCommits
+        self.actualVerifiedMerges = actualVerifiedMerges
         self.codeReviews = codeReviews
         self.pullRequests = pullRequests
-        self.noForceMerges = noForceMerges
+        self.blockForceMerges = blockForceMerges
 
 # Signature Verification (via Branch Protection Rules)
     def signed_commits(self):
     '''
-    If the API object indicates that the repostory requires signed commits, this will trigger a call to the Repository.signature_verification method with the value "true" to update the repo object.
+    If the API object indicates that the repostory requires signed commits, this will trigger a call to the Repository.signatures_required method with the value "true" to update the repo object.
     '''
-        if (self.signedCommits == true): 
-            repo.signature_verification(true)
+        if (self.reqsSignedCommits == true): 
+            repo.signatures_required(true)
+
+# Percentage of signed commits (via commit history)        
+    def signed_commits_actual(self):
+    '''
+    If the API object has data about the percentage of commits that are signed, will trigger a call to the Repo's signed_commits_percent method with the value.
+    '''
+        if (self.actualSignedCommits > 0):
+            repo.signed_commits_percent(self.actualSignedCommits)
 
 # Verified Merges vs. All Merges (?)
     def verified_merges(self):
      '''
     If the API object indicates that the repostory includes verified and signed merges, this will trigger a call to the Repository.full_attestation method with the percentage of merges that are verired and signed to update the repo object.
     '''
-        if (self.verifiedMerges > 0):
-            repo.full_attestation(verifiedMerges)
-
-# Secret Blocking (?)
-    def secret_scanning(self):
-     '''
-    If the API object indicates that the repostory performs scanning to block committing secrets, this will trigger a call to the Repository.secret_blocking method with the value "true" to update the repo object.
-    '''
-        if (self.secretBlocking == true):
-            repo.secret_blocking(true)
-
-# Security Scanning (?)
-    def security_scanning(self):
-     '''
-    If the API object indicates that the repostory performas automatic security scanning, this will trigger a call to the Repository.security_scanning method with the value "true" to update the repo object.
-    '''
-        if (self.securityScanning = true)
-            repo.security_scanning(true)
+        if (self.actualVerifiedMerges > 0):
+            repo.full_attestation_percent(self.actualVerifiedMerges)
 
 # Code Reviews Required (via Branch Protection Rules)
     def code_reviews(self):
@@ -72,7 +63,7 @@ class GHObject:
      '''
     If the API object indicates that the repostory blocks forced merges, this will trigger a call to the Repository.block_force_merge method with the value "true" to update the repo object.
     '''
-        if (self.noForceMerges == true):
+        if (self.blockForceMerges == true):
             repo.block_force_merges(true)
             
 # Full run to update a repo object:
@@ -81,10 +72,8 @@ class GHObject:
     This is a wrapper method which calls all the above methods in succession to trigger a full update of the repo object based on the API object. It first resets the repo values to zero/false, then updates all the values based on those in the apiObject and finally recomputes the repo's booleans based on the updated values.
     '''
         signed_commits()
+        signed_commits_actual()
         verified_merges()
-        secret_scanning()
-        security_scanning()
         code_reviews()
         pull_requests()
         no_force_merges()
-        repo.status_check()
