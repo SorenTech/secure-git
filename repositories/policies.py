93d76bfb485c8c1efbc1b8ae5cd70d0873077ab3
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 11:30:47 -0400

    Move policies to separate sub-module

--- repositories/policies.py
+++
@@ -1,9 +0,0 @@
-'''
-Establishes default policies for repository objects. May be overriden by the settings in config.py
-'''
-
-Merge_Attestation_Min = 0.75
-
-User_Auth_Min = 0.75
-
-Agent_Auth_MMin = 0.9
