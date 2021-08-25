6bd1c464874a812d0ceccfc1ad9b122054e419f0
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 09:38:14 -0400

    skeleton for repo initialization routine

--- library/init.py
+++ library/init.py
@@ -1,3 +1,8 @@
 '''
 Initialize a new sGit repository. Essentially just creates a repo object based on local repository details.
 '''
+
+def init_repo():
+    # Get PWD
+    # Find Git data
+    # Create Repo Object
