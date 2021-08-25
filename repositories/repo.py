5a1615fa7a6e47fdc1fc7c5589d24ad903a30095
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 13:48:22 -0400

    Drastically simplifying the repo object

--- repositories/repo.py
+++ repositories/repo.py
@@ -1,6 +1,5 @@
 # Establishes basic repository object and methods for manipulating its values.
 
-from Decimal import *
 import policies.repositories
 
 # Define the principle "repository" object that is used
@@ -28,66 +27,50 @@ class Repository:
         self.location = location
         self.users = []
         self.agents = []
-        self.hasSignedCommits = false
+        self.reqsSignedCommits = false
+        self.percentSignedCommits = 0.0
         self.hasFullAttestation = false
+        self.percentVerifiedMerges = 0.0
         self.hasSecretBlocking = false
         self.hasSecurityScanning = false
-        self.hasCodeReviews = false
-        self.hasPullRequests = false
-        self.hasNoForceMerges = false
-        self.hasUserAuth = false
-        self.hasAgentAuth = false
-        self.hasVerification = self.hasSignedCommits && self.hasFullAttestation
-        self.hasScanning = self.hasSecretBlocking && self.hasSecurityScanning
-        self.hasAuthorization = self.hasCodeReviews && self.hasPullRequests && self.hasNoForceMerges
-        self.hasAuthentication = self.hasUserAuth && self.hasAgentAuth
-        self.signedCommitsScore = 0.0
-        self.fullAttestationScore = 0.0
-        self.secretBlockingScore = 0.0
-        self.securityScanningScore = 0.0
-        self.codeReviewsScore = 0.0
-        self.pullRequestsScore = 0.0
-        self.forceMergesScore = 0.0
-        self.userAuthScore = 0.0
-        self.agentAuthScore = 0.0
-        self.verificationScore = self.signedCommitScore + self.fullAttestationScore
-        self.scanningScore = self.secretBlockingScore + self.securityScanningScore
-        self.authorizationScore = self.codeReviewScore + self.pullRequestScore + self.forceMergesScore
-        self.authenticationScore = self.userAuthScore + self.agentAuthScore
-        self.overallScore = Decimal.multiply(Decimal.add(verificationScore, scanningScore, protectionScore, authenticationScore), 1/4)
+        self.reqsCodeReviews = false
+        self.reqsPullRequests = false
+        self.noForceMerges = false
+       #  self.hasUserAuth = false
+       #  self.hasAgentAuth = false
         
 # Use Case: Check if Commit Signatures are Required
-    def signatures_required(self, reqsSignedCommits: bool):
+    def signatures_required(self, signedCommits: bool):
     '''
     Updates the hasSignedCommits value a repo based on whether that repo requires signed commits.
     '''
-        if (reqsSignedCommits == Signed_Commits):
-            self.hasSignedCommits = true
+        if (signedCommits == Signed_Commits):
+            self.reqsSignedCommits = true
         else:
-            self.hasSignedCommits = false
+            self.reqsSignedCommits = false
             
 # Use Case: Check % of Commits Signed
-    def signed_commits_percent(self, percentSignedCommits: float):
+    def signed_commits_percent(self, actualSignedCommits: float):
     '''
-    Updates the signedCommitsScore based on the percentage of commits to main branch which are signed.
+    Updates the percentSignedCommits value based on the percentage of commits to main branch which are signed.
     '''
-        if (percentSignedCommits > 0):
-            self.signedCommitsScore = percentSignedCommits
+        if (actualSignedCommits > 0):
+            self.percentSignedCommits = actualSignedCommits
 
 # Use Case: Check % of Verified and Signed Merges (ie, Full Attestation)
-    def full_attestation_percent(self, percentVerifiedMerges: float):
+    def full_attestation_percent(self, actualVerifiedMerges: float):
     '''
-    Updates the fullAttestationScore value of the repo based on the percentage of merges in the repo which are verified and signed.
+    Updates the percentVerfifiedMerges value of the repo based on the percentage of merges in the repo which are verified and signed.
     '''
-        if (percentVerifiedMerges > 0):
-            self.fullAttestationScore = percentVerifiedMerges
+        if (actualVerifiedMerges > 0):
+            self.percentVerfiedMerges = actualVerifiedMerges
             
 # Use Case: Set hasFullAttestation value vased on fullAttestationScore
     def full_attestation_bool(self):
     '''
-    Sets the hasFullAttestation value to "true" if fullAttestationScore is greater than 75%
+    Sets the hasFullAttestation value to "true" if percentVerifiedMerges is greater than polciy requirement
     '''
-        if (fullAttestationScore >= Merge_Attestation_Min):
+        if (percentVerifiedMerges >= Merge_Attestation_Min):
             self.hasFullAttestation = true
         else:
             self.hasFullAttestation = false
@@ -95,103 +78,49 @@ class Repository:
 # Use Case: Repo-Scanning -> Block Secrets Being Commmitted
     def secret_blocking(self, secretScanning: bool):
     '''
-    Updates the hasSecretBlocking and secretBlockingScore value based on whether or not automated scanning exists to block the committing of secret files to the repository.
+    Updates the hasSecretBlocking value based on whether or not automated scanning exists to block the committing of secret files to the repository.
     '''
         if (secretScanning == Secret_Scanning):
             self.hasSecretBlocking = true
-            self.secretBlockingScore = 1.0
         else:
             self.hasSecretBlocking = false
-            self.secretBlockingScore = 0.0
 
 ## Use Case: Repo-Scanning -> Automatic Security Scanning
     def security_scanning(self, securityScanning: bool):
     '''
-    Updates the hasSecurityScanning and securityScanningScore value of the repo based on whether or not automated security scanning is performed.
+    Updates the hasSecurityScanning value of the repo based on whether or not automated security scanning is performed.
     '''
         if (securityScanning == Security_Scanning):
             self.hasSecurityScanning = true
-            self.securityScanningScore = 1.0
         else:
             self.hasSecurityScanning = false
-            self.securityScanningScore = 0.0
 
 # Use Case: Repo-Authorizations -> Require Code Reviews
     def req_code_reviews(self, codeReviews: bool):
     '''
-    Updates the hasCodeReviews and codeReviewsScore value of the repo based on whether code reviews are required for merges into the main branch.
+    Updates the reqsCodeReviews value of the repo based on whether code reviews are required for merges into the main branch.
     '''
         if (codeReviews == Code_Reviews):
-            self.hasCodeReviews = true
-            self.codeReviewsScore = 1.0
+            self.reqsCodeReviews = true
         else:
-            self.hasCodeReviews = false
-            self.codeReviewsScore = 0.0
+            self.reqsCodeReviews = false
 
 # Use Case: Repo-Authorization -> Require Pull Requests on Main
     def req_pull_requests(self, pullRequests: bool):
     '''
-    Updates the hasPullRequests and pullRequestsScore values of the repo based on whether pull requests are required to merge code into the main branch.
+    Updates the reqsPullRequests values of the repo based on whether pull requests are required to merge code into the main branch.
     '''
         if (pullRequests == Pull_Requests):
-            self.hasPullRequests = true
-            self.pullRequestsScore = 1.0
+            self.reqsPullRequests = true
         else:
-            self.hasPullRequests = false
-            self.pullRequestsScore = 0.0
+            self.reqsPullRequests = false
 
 # Use Case: Repo-Authorizations -> Prevent Force-Merges 
-    def block_force_merges(self, noForceMerges: bool):
+    def block_force_merges(self, blockForceMerges: bool):
     '''
-    Updates the hasNoForceMerges and forceMergesScore values of the repo based on whether forced merges are blocked for all branches.
+    Updates the noForceMerges values of the repo based on whether forced merges are blocked for all branches.
     '''
-        if (noForceMerges == Blocked_Force_Merges):
-            self.hasNoForceMerges = true
-            self.forceMergesScore = 1.0
+        if (blockForceMerges == Blocked_Force_Merges):
+            self.noForceMerges = true
         else:
-            self.hasNoForceMerges = false
-            self.forceMergesScore = 0.0
-
-# Use Case: Repo-Authentication -> Users Meet Authentication Requirements
-    def user_authentication_score(self, users: dict):
-    '''
-    Updates the userAuthScore value of the repo based on the number of users who meet the authentication requirements.
-    '''
-        userAuthScores = []
-        userCount = len(users)
-        for user in users:
-            if (user.AuthMethodsApproved == True):
-                userAuthScores + 1
-        self.userAuthScore = Decimal.multiply(userAuthScores, 1/userCount)
-        
-# Use Case: Agent Auth Score -> Agent Auth Bool
-    def user_authentication_bool(self):
-    '''
-    Updates the hasUserAuth value if userAuthScore is greater than or equal to 75%
-    '''
-        if (self.userAuthScore >= User_Auth_Min):
-            self.hasUserAuth = true
-        else:
-            self.hasUserAuth = false
-
-# Use Case: Repo-Authentication -> Agents Meet Authentication Requirements
-    def agent_authentication_score(self, agents: dict):
-    '''
-    Updates the agentAuthScore value of the repo based on the number of automation agents who meet the authentication requirements.
-    '''
-        agentAuthScores = []
-        agentCount = len(agents)
-        for agent in agents:
-            if (agent.AuthMethodsApproved == True):
-                agentAuthScores + 1
-            self.authenticationScore = Decimall.multiply(agentAuthScores, 1/agentCount)
-            
-# Use Case: Agent Authentication Score -> Agent Auth Bool
-    def agent_authentication_bool(self):
-    '''
-    Updates the hasAgentAuth value to true if the agentAuthScore is greater than or equal to 90%
-    '''
-        if (self.agentAuthScore >= Agent_Auth_Min):
-            self.hasAgentAuth = true
-        else:
-            self.hasAgentAuth = false
+            self.noForceMerges = false
