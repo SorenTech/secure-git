5381f6a74a50bab3d3d8db036164e97c1916c7e9
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 14:57:50 -0400

    Comment syntax

--- library/check_repo.py
+++ library/check_repo.py
@@ -1,3 +1,21 @@
-'''
-Checks if a repository meets requirements. Returns updated scoring and bool values.
-'''
+# Checks if a repository meets requirements. Returns updated scoring and bool values.
+
+import repositories
+
+# Step 1: verify we have a valid repo object
+# - Get repo remote
+# - Check repo object storage for matching repo
+
+# Step 2: Run requisite query to get values from remote
+# - Determine remote host
+# - Run query based on remote host
+# - Run local queries
+
+# Step 3: Update repo values
+# - Pass return values to relevant adaptors
+# - Store updated values in the repo object storage
+
+# Step 4: Return results
+# - Determine areas where repo is deficient
+# - Format message
+# - Return message
