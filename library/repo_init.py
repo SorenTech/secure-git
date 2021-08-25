05ab2e45a9c0281097f6fb142148b85e96f188f4
Alex Floyd Marshall <apmarshall@soren.tech>
2021-08-25 11:36:53 -0400

    Basic skeleton of repo_init

--- library/repo_init.py
+++ library/repo_init.py
@@ -1,20 +1,24 @@
-'''
-Initialize a new sGit repository. Essentially just creates a repo object based on local repository details.
-'''
+# Initialize a new sGit repository. Essentially just creates a repo object based on local repository details.
 
 from pathlib import Path
+import repositories.repo
 
 root = Path('.')
 git = root / '.git' / 'remotes'
 
+repo_name = ""
+repo_stub = ""
+repo_loc = ""
+
 def get_repo_info(git):
     if (git.exists() && git.is_dir()):
-        with open() as info:
+        with open() as info: # to-do: add file with remote configs
             read_info = info.read()
             
         info.closed
     else:
-        # return error
+        # to-do: return error
 
 def init_repo():
-
+    repo = Repository(repo_name, repo_stub, repo_loc)
+    # to-do: persist this somewhere 
